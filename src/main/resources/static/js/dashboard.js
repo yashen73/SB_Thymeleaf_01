0e6d65a5760ac283c6dd201bef7deec7b90d5d40
yashen kavishka <yashenkavishkagallageg@gmail.com>
2026-03-16 09:33:08 +0530

    nn

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -8,7 +8,7 @@
                 clickable :true,
             }
         });
-    };
+
 
     var brand_swiper = new Swiper(".category-carousel", {
         slidesPerView: 6,
@@ -33,11 +33,9 @@
             },
         }
     });
-
-
+}
 
     initSwiper();
-    brand_swiper();
 })(jQuery);
 
 
@@ -46,15 +44,34 @@
 fetch("http://localhost:8080/item/showAllItems")
 .then(response => response.json())
 .then(data => {
-    let container = document.getElementById("itemcontainer");
+
+    let productitem = document.getElementById("product-grid");
+    let html ="";
 
     console.log(data)
+
+
     data.forEach(item =>{
-    let div = document.createElement("div")
-    div.innerHTML = "ID: " + item.item_id + ", Brand ID: " + item.brand_item_id + ", quantity: " + item.item_quantity;
+
     console.log( "ID: " + item.item_id + ", Brand ID: " + item.brand_item_id + ", quantity: " + item.item_quantity)
     console.log( "ID: " + item.item_id + ", Brand ID: " + item.brand_item_id + ", quantity: " + item.item_quantity)
 
-    container.appendChild(div);
+
+        html += `
+                <div class="col">
+                    <div class="product-item">
+                        <figure>
+                            <a href="#">
+                                <img src="images/product-thumbnails/item1_1.jpg" class="tab-image">
+                            </a>
+                        </figure>
+                        <h3>Shirt_01</h3>
+                        <span class="qty">20pcs</span>
+                        <span class="price">2500.00/=</span>
+                    </div>
+                </div>
+            `;
+
     })
+    productitem.innerHTML = html;
 })
